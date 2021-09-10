7b6e22a4caba075396578568d11f300b4588328f
Priyabrata Chattaraj <89516769+priyabratagig@users.noreply.github.com>
2021-09-10 07:33:07 +0000

    sendFile() first file responses introduced

--- myApp.js
+++ myApp.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
 console.log("Hello World");
-app.get("/",(req,res)=>{
-  res.send("Hello Express");
+app.get('/',(req,res)=>{
+  res.sendFile(__dirname + "/views/index.html");
 });
 
 
