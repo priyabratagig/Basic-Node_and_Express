d58c83c49694cd31b635bfc86dba3b2220c163b0
Priyabrata Chattaraj <89516769+priyabratagig@users.noreply.github.com>
2021-09-10 18:21:45 +0000

    .env variable added

--- myApp.js
+++ myApp.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var app = express();
 console.log("Hello World");
+
+app.use('/public',express.static(__dirname + '/public'));
+
 app.get('/',(req,res)=>{
   res.sendFile(__dirname + "/views/index.html");
+});
+
+app.get('/json',(req, res)=>{
+  if(process.env.MESSAGE_STYLE=="uppercase")
+    res.json({"message": "HELLO JSON"});
+  else
+    res.json({"message": "Hello json"});
 });
 
 
