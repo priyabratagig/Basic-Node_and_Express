7ca0e62a307e768a4bd1e7a95d06e6bf86c55b34
Priyabrata Chattaraj <89516769+priyabratagig@users.noreply.github.com>
2021-09-10 18:24:31 +0000

    middleware function root-label created

--- myApp.js
+++ myApp.js
@@ -2,6 +2,11 @@ var express = require('express');
 var app = express();
 console.log("Hello World");
 
+app.use((req, res, next)=>{
+  console.log(req.method+' '+req.path+" - "+req.ip)
+  next();
+});
+
 app.use('/public',express.static(__dirname + '/public'));
 
 app.get('/',(req,res)=>{
