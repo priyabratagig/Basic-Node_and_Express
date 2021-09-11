47a59c5b1d1a7c2964ee5bb9cac8b460e5bc1dca
Priyabrata Chattaraj <89516769+priyabratagig@users.noreply.github.com>
2021-09-11 08:35:51 +0000

    POST handler with body-parser middleware

--- myApp.js
+++ myApp.js
@@ -38,6 +38,8 @@ app.get('/:word/echo',function(req, res){
 
 app.route('/name').get((req, res)=>{
   res.json({'name': req.query.first+' '+req.query.last});
+  }).post(function(req, res){
+    res.json({'name': req.body.first + ' ' + req.body['last']});
   });
 
 
